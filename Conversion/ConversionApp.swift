ad31b4c19d8dd3ad03d237637c2ccaf7210b2712
Alexandr Rodionov <alexrodionby@gmail.com>
2023-05-01 23:00:16 +0300

    Initial Commit

---
+++ Conversion/ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionApp.swift
+//  Conversion
+//
+//  Created by Alexandr Rodionov on 1.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
